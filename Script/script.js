9809b616bb7ed925223eae51f8054183ec0e3731
Linda A <linda.29.andersson@gmail.com>
2021-12-07 18:23:28 +0100

    Polishing style and sort CSS files

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,7 @@
 const squares = document.querySelectorAll(".grid div");
 const result = document.querySelector("#result");
 const displayCurrentPlayer = document.querySelector("#current-player");
+const showColorDiv = document.querySelector("#showColor");
 let currentPlayer = 1;
 
 const winninArrays = [
@@ -85,13 +86,13 @@ function checkBoard() {
             square2.classList.contains("player-one") &&
             square3.classList.contains("player-one") &&
             square4.classList.contains("player-one")) {
-            result.innerText = "Player One Wins!";
+            result.innerText = "Player 1 Wins!";
         }
         if (square1.classList.contains("player-two") &&
             square2.classList.contains("player-two") &&
             square3.classList.contains("player-two") &&
             square4.classList.contains("player-two")) {
-            result.innerText = "Player Two Wins!";
+            result.innerText = "Player 2 Wins!";
         }
     }
 }
@@ -99,12 +100,16 @@ function checkBoard() {
 for (let i = 0; i < squares.length; i++) {
     squares[i].onclick = () => {
         if (squares[i + 7].classList.contains("taken") && !squares[i].classList.contains("taken")) {
-            if (currentPlayer === 1) {
+            if (currentPlayer === 1) {  
+                showColorDiv.classList.add("player-two");
+                showColorDiv.classList.remove("player-one");
                 squares[i].classList.add("taken"),
                     squares[i].classList.add("player-one");
                 currentPlayer = 2;
                 displayCurrentPlayer.innerText = currentPlayer;
             } else if (currentPlayer === 2) {
+                showColorDiv.classList.add("player-one");
+                showColorDiv.classList.remove("player-two");
                 squares[i].classList.add("taken"),
                     squares[i].classList.add("player-two");
                 currentPlayer = 1;
