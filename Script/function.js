f3fa3a04de18a4cc00acfb42cb8eb94380a77505
Linda A <linda.29.andersson@gmail.com>
2021-12-08 13:16:51 +0100

    Sort files and pop-up for winning

---
+++ Script/function.js
@@ -0,0 +1,22 @@
+function checkBoard() {
+    for (let i = 0; i < winninArrays.length; i++) {
+        const square1 = squares[winninArrays[i][0]];
+        const square2 = squares[winninArrays[i][1]];
+        const square3 = squares[winninArrays[i][2]];
+        const square4 = squares[winninArrays[i][3]];
+        if (square1.classList.contains("player-one") &&
+            square2.classList.contains("player-one") &&
+            square3.classList.contains("player-one") &&
+            square4.classList.contains("player-one")) {
+            result.innerText = "Player 1 Wins!";
+            result.classList.remove("hidden");
+        }
+        if (square1.classList.contains("player-two") &&
+            square2.classList.contains("player-two") &&
+            square3.classList.contains("player-two") &&
+            square4.classList.contains("player-two")) {
+            result.innerText = "Player 2 Wins!";
+            result.classList.remove("hidden");
+        }
+    }
+}
---
+++ Script/game.js
@@ -0,0 +1,22 @@
+for (let i = 0; i < squares.length; i++) {
+    squares[i].onclick = () => {
+        if (squares[i + 7].classList.contains("taken") && !squares[i].classList.contains("taken")) {
+            if (currentPlayer === 1) {
+                showColorDiv.classList.add("player-two");
+                showColorDiv.classList.remove("player-one");
+                squares[i].classList.add("taken"),
+                    squares[i].classList.add("player-one");
+                currentPlayer = 2;
+                displayCurrentPlayer.innerText = currentPlayer;
+            } else if (currentPlayer === 2) {
+                showColorDiv.classList.add("player-one");
+                showColorDiv.classList.remove("player-two");
+                squares[i].classList.add("taken"),
+                    squares[i].classList.add("player-two");
+                currentPlayer = 1;
+                displayCurrentPlayer.innerText = currentPlayer;
+            }
+
+        } checkBoard();
+    };
+}
