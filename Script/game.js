ca90e6c43779d808aabc471021b0197e1e8fd102
Linda A <linda.29.andersson@gmail.com>
2021-12-08 15:26:34 +0100

    Fixed so only one player can win

--- Script/game.js
+++ Script/game.js
@@ -1,5 +1,6 @@
 for (let i = 0; i < squares.length; i++) {
-    squares[i].onclick = () => {
+    squares[i].addEventListener("click", startClick);
+    function startClick() {
         if (squares[i + 7].classList.contains("taken") && !squares[i].classList.contains("taken")) {
             if (currentPlayer === 1) {
                 showColorDiv.classList.add("player-two");
@@ -16,7 +17,30 @@ for (let i = 0; i < squares.length; i++) {
                 currentPlayer = 1;
                 displayCurrentPlayer.innerText = currentPlayer;
             }
-
+        }
+        function checkBoard() {
+            for (let i = 0; i < winninArrays.length; i++) {
+                const square1 = squares[winninArrays[i][0]];
+                const square2 = squares[winninArrays[i][1]];
+                const square3 = squares[winninArrays[i][2]];
+                const square4 = squares[winninArrays[i][3]];
+                if (square1.classList.contains("player-one") &&
+                    square2.classList.contains("player-one") &&
+                    square3.classList.contains("player-one") &&
+                    square4.classList.contains("player-one")) {
+                    result.innerText = "Player 1 Wins!";
+                    result.classList.remove("hidden");
+                    break;
+                }
+                if (square1.classList.contains("player-two") &&
+                    square2.classList.contains("player-two") &&
+                    square3.classList.contains("player-two") &&
+                    square4.classList.contains("player-two")) {
+                    result.innerText = "Player 2 Wins!";
+                    result.classList.remove("hidden");
+                    break;
+                }
+            }
         } checkBoard();
-    };
+    }
 }
